444fe5be4ec9eb9e3e553e207cb72f35455534cd
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2023-05-03 23:15:38 +0900

    movements/edits

--- resources/views/front_end/index.blade.php
+++ resources/views/front_end/index.blade.php
@@ -64,44 +64,82 @@
 
         <main id="main">
 
-            <!-- ======= Featured Services Section ======= -->
-            <section id="featured-services" class="featured-services">
-                <div class="container">
+            <!-- ======= Services Section ======= -->
+            <section id="service" class="services pt-0">
+                <div class="container" data-aos="fade-up">
+
+                    <div class="section-header">
+                        <span>Church Ministry</span>
+                        <h2>Church Ministry</h2>
+
+                    </div>
 
                     <div class="row gy-4">
 
-                        <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up">
-                            <div class="icon flex-shrink-0"><i class="fa-solid fa-cart-flatbed"></i></div>
-                            <div>
-                                <h4 class="title">Lorem Ipsum</h4>
-                                <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-                                <a href="service-details.html" class="readmore stretched-link"><span>Learn More</span><i class="bi bi-arrow-right"></i></a>
+                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="assets/img/storage-service.jpg" alt="" class="img-fluid">
+                                </div>
+                                <h3><a href="service-details.html" class="stretched-link">Sermons (Change Picture)</a></h3>
+                                <p>In our church we regularly learn the word of God for instruction, encouragement and strengtheining our faith.</p>
                             </div>
-                        </div>
-                        <!-- End Service Item -->
+                        </div><!-- End Card Item -->
 
-                        <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="100">
-                            <div class="icon flex-shrink-0"><i class="fa-solid fa-truck"></i></div>
-                            <div>
-                                <h4 class="title">Dolor Sitema</h4>
-                                <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-                                <a href="service-details.html" class="readmore stretched-link"><span>Learn More</span><i class="bi bi-arrow-right"></i></a>
+                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="assets/img/logistics-service.jpg" alt="" class="img-fluid">
+                                </div>
+                                <h3><a href="service-details.html" class="stretched-link">Bible Study (Change Picture)</a></h3>
+                                <p>We throughly go through the different books of the bible by understanding their historical and literary context to find application in our lives.</p>
                             </div>
-                        </div><!-- End Service Item -->
+                        </div><!-- End Card Item -->
 
-                        <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="200">
-                            <div class="icon flex-shrink-0"><i class="fa-solid fa-truck-ramp-box"></i></div>
-                            <div>
-                                <h4 class="title">Sed ut perspiciatis</h4>
-                                <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-                                <a href="service-details.html" class="readmore stretched-link"><span>Learn More</span><i class="bi bi-arrow-right"></i></a>
+                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="assets/img/cargo-service.jpg" alt="" class="img-fluid">
+                                </div>
+                                <h3><a href="service-details.html" class="stretched-link">Worship (Change Picture)</a></h3>
+                                <p>Worshiping the Lord our God through music and hymns.</p>
                             </div>
-                        </div><!-- End Service Item -->
+                        </div><!-- End Card Item -->
+
+                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="assets/img/trucking-service.jpg" alt="" class="img-fluid">
+                                </div>
+                                <h3><a href="service-details.html" class="stretched-link">Prayer (Change Picture)</a></h3>
+                                <p>Coming to the throne of God to commune with our God and find Grace for our daily lives.</p>
+                            </div>
+                        </div><!-- End Card Item -->
+
+                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="assets/img/packaging-service.jpg" alt="" class="img-fluid">
+                                </div>
+                                <h3><a href="service-details.html" class="stretched-link">Expositions (Change Picture)</a></h3>
+                                <p> Expositions on Christian living, discipleship, etc.</p>
+                            </div>
+                        </div><!-- End Card Item -->
+
+                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+                            <div class="card">
+                                <div class="card-img">
+                                    <img src="assets/img/warehousing-service.jpg" alt="" class="img-fluid">
+                                </div>
+                                <h3><a href="service-details.html" class="stretched-link">Other Collections</a></h3>
+                                <p> Special Church events, birthdays, stories of our church members</p>
+                            </div>
+                        </div><!-- End Card Item -->
 
                     </div>
 
                 </div>
-            </section><!-- End Featured Services Section -->
+            </section><!-- End Services Section -->
 
             <!-- ======= About Us Section ======= -->
             <section id="about" class="about pt-0">
@@ -152,83 +190,6 @@
 
                 </div>
             </section><!-- End About Us Section -->
-
-            <!-- ======= Services Section ======= -->
-            <section id="service" class="services pt-0">
-                <div class="container" data-aos="fade-up">
-
-                    <div class="section-header">
-                        <span>Church Ministry</span>
-                        <h2>Church Ministry</h2>
-
-                    </div>
-
-                    <div class="row gy-4">
-
-                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
-                            <div class="card">
-                                <div class="card-img">
-                                    <img src="assets/img/storage-service.jpg" alt="" class="img-fluid">
-                                </div>
-                                <h3><a href="service-details.html" class="stretched-link">Sermons (Change Picture)</a></h3>
-                                <p>Cumque eos in qui numquam. Aut aspernatur perferendis sed atque quia voluptas quisquam repellendus temporibus itaqueofficiis odit</p>
-                            </div>
-                        </div><!-- End Card Item -->
-
-                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
-                            <div class="card">
-                                <div class="card-img">
-                                    <img src="assets/img/logistics-service.jpg" alt="" class="img-fluid">
-                                </div>
-                                <h3><a href="service-details.html" class="stretched-link">Bible Study (Change Picture)</a></h3>
-                                <p>Asperiores provident dolor accusamus pariatur dolore nam id audantium ut et iure incidunt molestiae dolor ipsam ducimus occaecati nisi</p>
-                            </div>
-                        </div><!-- End Card Item -->
-
-                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
-                            <div class="card">
-                                <div class="card-img">
-                                    <img src="assets/img/cargo-service.jpg" alt="" class="img-fluid">
-                                </div>
-                                <h3><a href="service-details.html" class="stretched-link">Worship (Change Picture)</a></h3>
-                                <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p>
-                            </div>
-                        </div><!-- End Card Item -->
-
-                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
-                            <div class="card">
-                                <div class="card-img">
-                                    <img src="assets/img/trucking-service.jpg" alt="" class="img-fluid">
-                                </div>
-                                <h3><a href="service-details.html" class="stretched-link">Prayer (Change Picture)</a></h3>
-                                <p>Dicta quam similique quia architecto eos nisi aut ratione aut ipsum reiciendis sit doloremque oluptatem aut et molestiae ut et nihil</p>
-                            </div>
-                        </div><!-- End Card Item -->
-
-                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
-                            <div class="card">
-                                <div class="card-img">
-                                    <img src="assets/img/packaging-service.jpg" alt="" class="img-fluid">
-                                </div>
-                                <h3><a href="service-details.html" class="stretched-link">Expositions (Change Picture)</a></h3>
-                                <p> Expositions on Christian living, discipleship, etc.</p>
-                            </div>
-                        </div><!-- End Card Item -->
-
-                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
-                            <div class="card">
-                                <div class="card-img">
-                                    <img src="assets/img/warehousing-service.jpg" alt="" class="img-fluid">
-                                </div>
-                                <h3><a href="service-details.html" class="stretched-link">Other Collections</a></h3>
-                                <p> Here post videos on Church events, birthdays, </p>
-                            </div>
-                        </div><!-- End Card Item -->
-
-                    </div>
-
-                </div>
-            </section><!-- End Services Section -->
 
             <!-- ======= Call To Action Section ======= -->
             <section id="call-to-action" class="call-to-action">
